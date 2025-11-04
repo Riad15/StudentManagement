e568cf25d7974aa46c78e7a47579623ac29d60ba
Asif Riad <asifriad15@gmail.com>
2025-11-04 21:40:42 +0600

    Create a model student  classs

---
+++ StudentManagement.Model/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentManagement.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Department { get; set; }
+
+    }
+}
