ebbc27bac87f8c28d1f6ca68cf436f8791a3e33f
Asif Riad <asifriad15@gmail.com>
2025-11-04 21:44:45 +0600

    Create A DBContext Class

---
+++ StudentManagement.DAL/StudentManagementDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Model;
+
+namespace StudentManagement.DAL
+{
+    public class StudentManagementDBContext : DbContext
+    {
+
+        public StudentManagementDBContext(DbContextOptions<StudentManagementDBContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
