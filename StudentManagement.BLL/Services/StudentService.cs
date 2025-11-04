c0c1efb9cfd57bd5827fa3073897c0f66e39178c
Asif Riad <asifriad15@gmail.com>
2025-11-04 21:51:01 +0600

    Create server management and model menegment entity

---
+++ StudentManagement.BLL/Services/StudentService.cs
@@ -0,0 +1,26 @@
+﻿using StudentManagement.DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.BLL.Services
+{
+    public class StudentService
+    {
+
+        private readonly StudentRepository _studentRepository;
+
+        public StudentService(StudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+
+        public IEnumerable<Model.Student> GetAllStudents()
+        {
+            return _studentRepository.GetAllStudents();
+        }
+    }
+}
---
+++ StudentManagement.DAL/Repository/StudentRepository.cs
@@ -0,0 +1,25 @@
+﻿using StudentManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.DAL.Repository
+{
+    public class StudentRepository
+    {
+        private readonly StudentManagementDBContext _context;
+
+        public StudentRepository(StudentManagementDBContext context)
+        {
+            _context = context;
+        }
+
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return _context.Students.ToList();
+        }
+    }
+}
