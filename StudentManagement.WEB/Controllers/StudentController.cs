afdd7545a1623fe005ec61f81a32a84a34d9196d
Asif Riad <asifriad15@gmail.com>
2025-11-04 22:32:59 +0600

    add student list operation

---
+++ StudentManagement.WEB/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagement.BLL.Services;
+
+namespace StudentManagement.WEB.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private readonly StudentService _studentService;
+
+        public StudentController(StudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        public IActionResult Index()
+        {
+
+            var students = _studentService.GetAllStudents();
+            return View(students);
+        }
+    }
+}
